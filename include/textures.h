a9a3803ecbcf6c1b91bb720ac7efce605a8ed3d3
sl4Zone <48824808+sl4Zone@users.noreply.github.com>
2021-02-18 18:19:10 +0100

    Moving some code from textures.h
    Moving some code from textures.h to textures.c so it compiles.
    This was that making stuff work meant

--- include/textures.h
+++ include/textures.h
@@ -3,7 +3,7 @@
 
 #include <vita2d.h>
 
-vita2d_texture *icon_dir, *icon_file, *icon_audio, *battery_20, *battery_20_charging, *battery_30, *battery_30_charging, *battery_50, *battery_50_charging, \
+extern vita2d_texture *icon_dir, *icon_file, *icon_audio, *battery_20, *battery_20_charging, *battery_30, *battery_30_charging, *battery_50, *battery_50_charging, \
 	*battery_60, *battery_60_charging, *battery_80, *battery_80_charging, *battery_90, *battery_90_charging, *battery_full, *battery_full_charging, \
 	*battery_low, *battery_unknown, *default_artwork, *default_artwork_blur, *btn_play, *btn_pause, *btn_rewind, *btn_forward, *btn_repeat, \
 	*btn_shuffle, *btn_repeat_overlay, *btn_shuffle_overlay, *icon_back, *toggle_on, *toggle_off, *radio_on, *radio_off;
